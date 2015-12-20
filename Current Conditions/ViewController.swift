f28ed020a52f8842c7a31da5c843234cb867e6d4
Swapnil Beta <swapnildhanwal@hotmail.com>
2015-12-21 00:58:33 +0530

    Initial Commit

---
+++ Current Conditions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Current Conditions
+//
+//  Created by Swapnil Dhanwal on 21/12/15.
+//  Copyright © 2015 SwApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
